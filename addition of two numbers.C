225c91701a30ffbb4ebd1aaf6efd3ce1ad21f12d
wajid-9 <124772927+wajid-9@users.noreply.github.com>
2023-08-08 21:34:16 +0530

    Add files via upload

---
+++ addition of two numbers.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+int a,b,sum;
+printf("Enter the number a:");
+scanf("%d",&a);
+printf("Enter the number b:");
+scanf("%d",&b);
+sum=a+b;
+printf("The addition of given numbers %d",sum);
+getch();
+return 0;
+}
